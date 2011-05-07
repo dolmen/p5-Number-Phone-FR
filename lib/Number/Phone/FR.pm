0e7d215bce87c29e920deae8491e7487b489fdcf
Olivier Mengué <dolmen@cpan.org>
2011-05-07 15:55:44 +0200

    Full.pm: fix anchoring in operator lookup regexp
    The fix is in the generator template.
    Bump version number.
    Update 'Changes'.

--- lib/Number/Phone/FR.pm
+++ lib/Number/Phone/FR.pm
@@ -7,7 +7,7 @@ package Number::Phone::FR;
 # $VERSION is limited to 2 digits after the dot
 # Other digits are reserved for ARCEP data versonning
 # in Number::Phone::FR::Full
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Number::Phone;
 use parent 'Number::Phone';
