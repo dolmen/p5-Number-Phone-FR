696cf403019b89495d0aba36b74c28a222082896
Olivier Mengué <dolmen@cpan.org>
2011-09-03 20:34:10 +0200

    Ready for release 0.0511217

--- lib/Number/Phone/FR.pm
+++ lib/Number/Phone/FR.pm
@@ -7,7 +7,7 @@ package Number::Phone::FR;
 # $VERSION is limited to 2 digits after the dot
 # Other digits are reserved for ARCEP data versonning
 # in Number::Phone::FR::Full
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use Number::Phone;
 use parent 'Number::Phone';
