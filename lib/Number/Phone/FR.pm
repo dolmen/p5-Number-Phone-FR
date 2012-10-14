a6b638fa8dcdf23bdb58cf63ca18968a3229ef1f
Olivier Mengué <dolmen@cpan.org>
2012-10-14 23:45:50 +0200

    Changes for release 0.0712272
    Distribution dédicacée à Soline pour son mensuversaire.

--- lib/Number/Phone/FR.pm
+++ lib/Number/Phone/FR.pm
@@ -7,7 +7,7 @@ package Number::Phone::FR;
 # $VERSION is limited to 2 digits after the dot
 # Other digits are reserved for ARCEP data versonning
 # in Number::Phone::FR::Full
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Number::Phone;
 use parent 'Number::Phone';
